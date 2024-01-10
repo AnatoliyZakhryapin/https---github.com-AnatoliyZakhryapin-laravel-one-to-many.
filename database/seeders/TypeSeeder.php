273a7f880d75bb40669ba3062687b980a07a0791
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-10 17:24:39 +0100

    ADD TypeSeeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['FrontEnd','BackEnd','FullStack','Design','DevOps'];
+
+        foreach ($types as $type_name) {
+            
+            $new_type = new Type();
+            $new_type->name = $type_name;
+            $new_type->slug = Str::slug($type_name);
+
+            $new_type->save();
+        }
     }
 }
